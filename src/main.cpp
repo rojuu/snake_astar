22b2a45ee5d55e0358297797fcd683a162eef161
Roni Juppi <roni.juppi@gmail.com>
2018-03-27 22:39:54 +0300

    Deleted some code. Drawing some rects. Everything is in main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -18,21 +18,47 @@
  * If we'd do that, we could use some useful windows API calls.
  * Maybe still use SDL, but only use those calls for useful stuff?
  * How would I go about portability in a situation like that?
- * 
- **/
+ *
+**/
 
 #define internal static
 
-//#define arrayCount(arr) (sizeof(arr)/sizeof(*arr))
 #define array_count(x) ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
 
-#include "types.cpp"
-#include "math.cpp"
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float f32;
+typedef double f64;
+
+typedef i8 b8;
+typedef i16 b16;
+typedef i32 b32;
+typedef i64 b64;
+
+typedef glm::vec2 v2;
+typedef glm::vec3 v3;
+typedef glm::vec4 v4;
+
+typedef glm::mat2 m2;
+typedef glm::mat3 m3;
+typedef glm::mat4 m4;
 
 internal const f32 PI = glm::pi<f32>();
 
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 768
+const i32 SCREEN_WIDTH = 512;
+const i32 SCREEN_HEIGHT = 512;
+
+struct Position {
+    i32 x, y;
+};
 
 i32
 main(i32 argc, char **argv) {
@@ -48,7 +74,7 @@ main(i32 argc, char **argv) {
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         SCREEN_WIDTH, SCREEN_HEIGHT,
-        0);//SDL_WINDOW_OPENGL);
+        0);
 
     SDL_Renderer *renderer = SDL_CreateRenderer(
         window, -1, SDL_RENDERER_ACCELERATED);
@@ -58,18 +84,30 @@ main(i32 argc, char **argv) {
         return -1;
     }
 
-    const int snake_cell_width = 124;
-    const int snake_cell_height = 124;
-    SDL_Rect rects[] {
-        {
-            SCREEN_WIDTH/2,
-            SCREEN_HEIGHT/2,
-            snake_cell_width,
-            snake_cell_height
-        }
-    };
+    const i32 grid_size = 32;
 
-    const int rect_count = array_count(rects);
+    const i32 snake_cell_width = SCREEN_WIDTH / grid_size;
+    const i32 snake_cell_height = SCREEN_HEIGHT / grid_size;
+
+    const i32 max_snake_cell_count = grid_size * grid_size;
+
+    SDL_Rect rects[max_snake_cell_count];
+    Position positions[max_snake_cell_count];
+
+    for(i32 i = 0; i < max_snake_cell_count; i++) {
+        rects[i].w = snake_cell_width;
+        rects[i].h = snake_cell_height;
+        rects[i].x = positions[i].x = 0;
+        rects[i].y = positions[i].y = 0;
+    }
+
+    rects[1].x = 1 * snake_cell_width;
+    rects[1].y = 0.5f * snake_cell_height;
+
+    rects[2].x = 2 * snake_cell_width;
+    rects[2].y = 0 * snake_cell_height;
+
+    i32 cell_count = 1;
 
     b32 running = true;
     f64 current_time = (f32)SDL_GetPerformanceCounter() /
@@ -85,7 +123,7 @@ main(i32 argc, char **argv) {
                        (f64)SDL_GetPerformanceFrequency();
         delta_time = (f64)(current_time - last_time);
 
-        // Count frames for every second and print it as the title of the window
+        // Count frames for every second and pri32 it as the title of the window
         ++frame_counter;
         if (current_time >= (last_fps_time + 1.f)) {
             last_fps_time = current_time;
@@ -115,11 +153,18 @@ main(i32 argc, char **argv) {
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderFillRects(renderer, rects, rect_count);
+        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+        //SDL_RenderFillRects(renderer, rects, cell_count);
+        SDL_RenderFillRect(renderer, &rects[0]);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+        SDL_RenderFillRect(renderer, &rects[1]);
+        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        SDL_RenderFillRect(renderer, &rects[2]);
+
+
 
         SDL_RenderPresent(renderer);
     }
