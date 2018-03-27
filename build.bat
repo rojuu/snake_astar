bd1a5d6fef784bfe3d32dc0888e489962b554ddc
Roni Juppi <roni.juppi@gmail.com>
2018-03-27 21:55:50 +0300

    Removed OpenGL, because SDL renderer should be enough for us. Removed unused libraries as well.

--- build.bat
+++ build.bat
@@ -6,16 +6,11 @@ set SDL2=%CD%\libs\SDL2-2.0.5
 set SDL_INC=%SDL2%\include
 set SDL_LIB=%SDL2%\lib\%TARGET%
 
-set GLEW=%CD%\libs\glew-2.0.0
-set GLEW_BIN=%GLEW%\bin\Release\%TARGET%
-set GLEW_INC=%GLEW%\include
-set GLEW_LIB=%GLEW%\lib\Release\%TARGET%
-
 set GLM_INC=%CD%\libs\glm-0.9.8.4\glm
 
-set CommonCompilerFlags=/Zi /Za /Od /EHsc /nologo /FC /I%SDL_INC% /I%GLEW_INC% /I%GLM_INC%
+set CommonCompilerFlags=/Zi /Za /Od /EHsc /nologo /FC /I%SDL_INC% /I%GLM_INC%
 
-set CommonLinkerFlags=/DEBUG /LIBPATH:%SDL_LIB% /LIBPATH:%GLEW_LIB% SDL2.lib SDL2main.lib glew32.lib opengl32.lib
+set CommonLinkerFlags=/DEBUG /LIBPATH:%SDL_LIB% SDL2.lib SDL2main.lib
 
 if not exist bin (
     mkdir bin
